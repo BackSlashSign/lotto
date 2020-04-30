7d8d3bb0703c03a373c9afbc5389046df4ef5c14
padosae <jsm9943@naver.com>
2020-04-30 18:16:01 +0900

    new file:

---
+++ UpWind.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpWind : MonoBehaviour
+{
+    public float force = 1000.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("trigger called");
+        other.gameObject.GetComponent<Rigidbody>().AddForce(0, force, 0);
+    }
+}
